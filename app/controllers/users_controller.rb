14be3e38fad8251e56b739e4c5e9fb0e29698b50
hokmwong5 <hokmanwog@gmail.com>
2017-11-09 20:57:43 +0800

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ before_action :admin_user, only: :destroy
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless user.activated
   end
 
   def new
@@ -14,9 +15,10 @@ before_action :admin_user, only: :destroy
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account"
+      redirect_to root_url
     else
       render 'new'
     end
@@ -37,7 +39,7 @@ before_action :admin_user, only: :destroy
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ post '/login', to: 'sessions#create'
 delete '/logout', to: 'sessions#destroy'
 
 resources :users
+resources :account_activations, only: [:edit]
 
 end
